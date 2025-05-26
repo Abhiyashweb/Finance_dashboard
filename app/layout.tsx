513db9d7a6368aeedf97782028261a6a06d5d82c
Abhiyashweb <122610437+Abhiyashweb@users.noreply.github.com>
2025-05-26 10:08:21 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,8 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Finance
- Dashboard',
-    default: 'Finance
- Dashboard',
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
